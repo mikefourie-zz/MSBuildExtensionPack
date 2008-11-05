a15d5dc8ea180d9961ccd5037ff47ae4865073d2
mikeFourie_cp <SND\mikeFourie_cp>
2008-11-05 11:11:05 +0000

    Compression.Zip: Fixed bug where optional RemoveRoot caused incorrect results if not supplied. Changed the exposed properties to be ITaskItems to enable batching. Updated documentation and sample.

--- Solutions/Main3.5/MSBuildJSharp/Zip.cs
+++ Solutions/Main3.5/MSBuildJSharp/Zip.cs
@@ -25,17 +25,44 @@ namespace MSBuild.ExtensionPack.Compression
     ///         <TPath Condition="Exists('$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks')">$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks</TPath>
     ///     </PropertyGroup>
     ///     <Import Project="$(TPath)"/>
-    ///     <Target Name="Default">
+    ///     <Target Name="Default" DependsOnTargets="Sample1;Sample2"/>
+    ///     <Target Name="Sample1">
     ///         <ItemGroup>
     ///             <!-- Set the collection of files to Zip-->
     ///             <FilesToZip Include="C:\hotfixes\**\*"/>
     ///         </ItemGroup>
     ///         <!-- Create a zip file based on the FilesToZip collection -->
-    ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Create" CompressFiles="@(FilesToZip)" RemoveRoot="C:\hotfixes" ZipFileName="C:\newZipByFile.zip"/>
+    ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Create" CompressFiles="@(FilesToZip)" RemoveRoot="C:\hotfixes\" ZipFileName="C:\newZipByFile.zip"/>
     ///         <!-- Create a zip file based on a Path -->
-    ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Create" CompressPath="C:\hotfixes" RemoveRoot="C:\hotfixes" ZipFileName="C:\newZipByPath.zip"/>
+    ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Create" CompressPath="C:\hotfixes" RemoveRoot="C:\hotfixes\" ZipFileName="C:\newZipByPath.zip"/>
     ///         <!-- Extract a zip file-->
     ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Extract" ExtractPath="C:\aaa11" ZipFileName="C:\newZipByPath.zip"/>
+    ///     </Target>
+    ///     <Target Name="Sample2">
+    ///         <PropertyGroup>
+    ///             <SourceDirectory>MotorData\</SourceDirectory>
+    ///         </PropertyGroup>
+    ///         <ItemGroup>
+    ///             <Files Include="$(SourceDirectory)*" Exclude="$(SourceDirectory).XYZ\**\*">
+    ///                 <Group>Common</Group>
+    ///             </Files>
+    ///             <Files Include="$(SourceDirectory)Cars\*" Exclude="$(SourceDirectory)Cars\.XYZ\**\*">
+    ///                 <Group>Cars</Group>
+    ///             </Files>
+    ///             <Files Include="$(SourceDirectory)Trucks\*" Exclude="$(SourceDirectory)Trucks\.XYZ\**\*">
+    ///                 <Group>Trucks</Group>
+    ///             </Files>
+    ///         </ItemGroup>
+    ///         <!-- Create the output folder -->
+    ///         <ItemGroup>
+    ///             <OutputDirectory Include="output\"/>
+    ///         </ItemGroup>
+    ///         <MakeDir Directories="@(OutputDirectory)"/>
+    ///         <PropertyGroup>
+    ///             <WorkingDir>%(OutputDirectory.Fullpath)</WorkingDir>
+    ///         </PropertyGroup>
+    ///         <!-- Zip files based on the group they belong to -->
+    ///         <MSBuild.ExtensionPack.Compression.Zip TaskAction="Create" CompressFiles="@(Files)" ZipFileName="$(WorkingDir)%(Files.Group).zip"/>
     ///     </Target>
     /// </Project>
     /// ]]></code>    
@@ -52,23 +79,23 @@ namespace MSBuild.ExtensionPack.Compression
         /// <summary>
         /// Sets the Path to Zip.
         /// </summary>
-        public string CompressPath { get; set; }
+        public ITaskItem CompressPath { get; set; }
 
         /// <summary>
-        /// Sets the root to remove from the zip path
+        /// Sets the root to remove from the zip path. Note that this should be part of the file to compress path, not the target path of the ZipFileName
         /// </summary>
-        public string RemoveRoot { get; set; }
+        public ITaskItem RemoveRoot { get; set; }
 
         /// <summary>
         /// Sets the name of the Zip File
         /// </summary>
         [Required]
-        public string ZipFileName { get; set; }
+        public ITaskItem ZipFileName { get; set; }
 
         /// <summary>
         /// Path to extract the zip file to
         /// </summary>
-        public string ExtractPath { get; set; }
+        public ITaskItem ExtractPath { get; set; }
 
         /// <summary>
         /// This is the main InternalExecute method that all tasks should implement
@@ -107,22 +134,26 @@ namespace MSBuild.ExtensionPack.Compression
         private void Create()
         {
             this.Log.LogMessage(string.Format(CultureInfo.CurrentCulture, "Creating ZipFile: {0}", this.ZipFileName));
-            this.zos = new ZipOutputStream(new java.io.FileOutputStream(this.ZipFileName));
+            this.zos = new ZipOutputStream(new java.io.FileOutputStream(this.ZipFileName.GetMetadata("FullPath")));
             try
             {
                 if (this.CompressFiles != null)
                 {
                     foreach (ITaskItem f in this.CompressFiles)
                     {
-                        string filePath = f.ItemSpec;
-                        string zipentry = string.Empty;
-                        if (!string.IsNullOrEmpty(this.RemoveRoot))
+                        string filePath = f.GetMetadata("FullPath");
+                        string zipentry;
+                        if (this.RemoveRoot != null)
                         {
-                            zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(Path.GetFullPath(this.RemoveRoot).Length), Path.GetFileName(filePath));
+                            zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(Path.GetFullPath(this.RemoveRoot.GetMetadata("FullPath")).Length - 1), Path.GetFileName(filePath));
                             if (zipentry[0] == Path.DirectorySeparatorChar)
                             {
                                 zipentry = zipentry.Substring(1);
                             }
+                        }
+                        else
+                        {
+                            zipentry = Path.GetFileName(filePath);
                         }
 
                         ZipEntry z = new ZipEntry(zipentry);
@@ -147,10 +178,9 @@ namespace MSBuild.ExtensionPack.Compression
                         }
                     }
                 }
-                else if (!string.IsNullOrEmpty(this.CompressPath))
+                else if (this.CompressPath != null)
                 {
-                    DirectoryInfo rootDirectory = new DirectoryInfo(this.CompressPath);
-
+                    DirectoryInfo rootDirectory = new DirectoryInfo(this.CompressPath.GetMetadata("FullPath"));
                     FileSystemInfo[] infos = rootDirectory.GetFileSystemInfos("*");
                     this.ProcessFolder(infos);
                 }
@@ -176,14 +206,18 @@ namespace MSBuild.ExtensionPack.Compression
                 {
                     // add the folder
                     string filePath = i.FullName;
-                    string zipentry = string.Empty;
-                    if (!string.IsNullOrEmpty(this.RemoveRoot))
+                    string zipentry;
+                    if (this.RemoveRoot != null)
                     {
-                        zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(Path.GetFullPath(this.RemoveRoot).Length), Path.GetFileName(filePath));
+                        zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(this.RemoveRoot.GetMetadata("FullPath").Length - 1), Path.GetFileName(filePath));
                         if (zipentry[0] == Path.DirectorySeparatorChar)
                         {
                             zipentry = zipentry.Substring(1);
                         }
+                    }
+                    else
+                    {
+                        zipentry = Path.GetFileName(filePath);
                     }
 
                     // add the / so that empty folders are added too
@@ -202,14 +236,18 @@ namespace MSBuild.ExtensionPack.Compression
                 else if (i is FileInfo)
                 {
                     string filePath = i.FullName;
-                    string zipentry = string.Empty;
-                    if (!string.IsNullOrEmpty(this.RemoveRoot))
+                    string zipentry;
+                    if (this.RemoveRoot != null)
                     {
-                        zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(Path.GetFullPath(this.RemoveRoot).Length), Path.GetFileName(filePath));
+                        zipentry = Path.Combine(Path.GetDirectoryName(filePath).Substring(this.RemoveRoot.GetMetadata("FullPath").Length - 1), Path.GetFileName(filePath));
                         if (zipentry[0] == Path.DirectorySeparatorChar)
                         {
                             zipentry = zipentry.Substring(1);
                         }
+                    }
+                    else
+                    {
+                        zipentry = Path.GetFileName(filePath);
                     }
 
                     ZipEntry z = new ZipEntry(zipentry);
@@ -238,27 +276,27 @@ namespace MSBuild.ExtensionPack.Compression
 
         private void Extract()
         {
-            if (!File.Exists(this.ZipFileName))
+            if (!File.Exists(this.ZipFileName.GetMetadata("FullPath")))
             {
                 Log.LogError(string.Format(CultureInfo.CurrentCulture, "ZipFileName not found: {0}", this.ZipFileName));
                 return;
             }
 
-            if (string.IsNullOrEmpty(this.ExtractPath))
+            if (string.IsNullOrEmpty(this.ExtractPath.GetMetadata("FullPath")))
             {
                 Log.LogError("ExtractPath is required");
                 return;
             }
 
             this.Log.LogMessage(string.Format(CultureInfo.CurrentCulture, "Extracting ZipFile: {0} to: {1}", this.ZipFileName, this.ExtractPath));
-            ZipFile zf = new ZipFile(this.ZipFileName);
+            ZipFile zf = new ZipFile(this.ZipFileName.GetMetadata("FullPath"));
             foreach (ZipEntry zipEntry in new EnumerationWrapper(zf.entries()))
             {
                 java.io.InputStream s = zf.getInputStream(zipEntry);
                 try
                 {
                     string fname = System.IO.Path.GetFileName(zipEntry.getName());
-                    string newpath = System.IO.Path.Combine(this.ExtractPath, System.IO.Path.GetDirectoryName(zipEntry.getName()));
+                    string newpath = System.IO.Path.Combine(this.ExtractPath.GetMetadata("FullPath"), System.IO.Path.GetDirectoryName(zipEntry.getName()));
                     Directory.CreateDirectory(newpath);
                     if (!zipEntry.isDirectory())
                     {
