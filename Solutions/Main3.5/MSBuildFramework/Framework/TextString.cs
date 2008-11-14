7356ffdcc38e102093f639bbd2e3176a588f3e5c
mikeFourie_cp <SND\mikeFourie_cp>
2008-11-14 10:05:04 +0000

    Framework.TextString.cs: New TaskActions - EndsWith, PadLeft, PadRight, StartsWith, ToLower and ToUpper. Updated documentation XmlSamples\TextString.proj: Updated sample
    Resolves #967 (Add ToUpper and ToLower Actions to TextString) requested by dwhearn on 13 November 2008

--- Solutions/Main3.5/MSBuildFramework/Framework/TextString.cs
+++ Solutions/Main3.5/MSBuildFramework/Framework/TextString.cs
@@ -10,22 +10,58 @@ namespace MSBuild.ExtensionPack.Framework
     /// <summary>
     /// <b>Valid TaskActions are:</b>
     /// <para><i>Compare</i> (<b>Required: </b> String1, String2, Comparison <b> Optional: </b> IgnoreCase <b>Output: </b>Result)</para>
+    /// <para><i>EndsWith</i> (<b>Required: </b> String1, String2<b> Optional: </b> IgnoreCase <b>Output: </b>Result)</para>
     /// <para><i>GetLength</i> (<b>Required: </b> OldString<b> Output: </b> NewString)</para>
     /// <para><i>Insert</i> (<b>Required: </b> OldString, String1, StartIndex<b> Output: </b> NewString)</para>
+    /// <para><i>PadLeft</i> (<b>Required: </b> OldString, String1 (1 char) <b> Optional: </b>Count <b>Output: </b> NewString)</para>
+    /// <para><i>PadRight</i> (<b>Required: </b> OldString, String1 (1 char) <b> Optional: </b>Count <b>Output: </b> NewString)</para>
     /// <para><i>Remove</i> (<b>Required: </b> OldString, StartIndex <b> Optional: </b>Count <b> Output: </b> NewString)</para>
     /// <para><i>Replace</i> (<b>Required: </b> OldString, OldValue, NewValue <b> Output: </b> NewString)</para>
+    /// <para><i>StartsWith</i> (<b>Required: </b> String1, String2<b> Optional: </b> IgnoreCase <b>Output: </b>Result)</para>
+    /// <para><i>ToLower</i> (<b>Required: </b> OldString<b> Output: </b> NewString)</para>
+    /// <para><i>ToUpper</i> (<b>Required: </b> OldString<b> Output: </b> NewString)</para>
     /// <para><i>Trim</i> (<b>Required: </b> OldString<b> Output: </b> NewString)</para>
     /// <para><b>Remote Execution Support:</b> NA</para>
     /// </summary>
     /// <example>
     /// <code lang="xml"><![CDATA[
-    ///  <Project ToolsVersion="3.5" DefaultTargets="Default" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+    /// <Project ToolsVersion="3.5" DefaultTargets="Default" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
     ///     <PropertyGroup>
     ///         <TPath>$(MSBuildProjectDirectory)\..\MSBuild.ExtensionPack.tasks</TPath>
     ///         <TPath Condition="Exists('$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks')">$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks</TPath>
     ///     </PropertyGroup>
     ///     <Import Project="$(TPath)"/>
     ///     <Target Name="Default">
+    ///         <!-- Uppercase a string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="ToUpper" OldString="helLo">
+    ///             <Output PropertyName="out" TaskParameter="NewString"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(out)"/>
+    ///         <!-- Lowercase a string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="ToLower" OldString="HellO">
+    ///             <Output PropertyName="out" TaskParameter="NewString"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(out)"/>
+    ///         <!-- PadLeft a string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="PadLeft" OldString="Hello" String1="A" Count="10">
+    ///             <Output PropertyName="out" TaskParameter="NewString"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(out)"/>
+    ///         <!-- PadRight a string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="PadRight" OldString="Hello" String1="A" Count="10">
+    ///             <Output PropertyName="out" TaskParameter="NewString"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(out)"/>
+    ///         <!-- Check whether a string starts with another string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="StartsWith" String1="Hello" String2="He">
+    ///             <Output PropertyName="TheResult" TaskParameter="Result"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(TheResult)"/>
+    ///         <!-- Check whether a string ends with another string -->
+    ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="EndsWith" String1="Hello" String2="Lo" IgnoreCase="false">
+    ///             <Output PropertyName="TheResult" TaskParameter="Result"/>
+    ///         </MSBuild.ExtensionPack.Framework.TextString>
+    ///         <Message Text="The Result: $(TheResult)"/>
     ///         <!-- Compare two strings to see whether they are equal -->
     ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="Compare" String1="Hello" String2="Hello" Comparison="equals">
     ///             <Output PropertyName="TheResult" TaskParameter="Result"/>
@@ -53,9 +89,9 @@ namespace MSBuild.ExtensionPack.Framework
     ///         <Message Text="The Result: $(TheResult)"/>
     ///         <!-- Replace the contents off a string -->
     ///         <MSBuild.ExtensionPack.Framework.TextString TaskAction="Replace" OldString="Hello" OldValue="llo" NewValue="XYZ">
-    ///             <Output PropertyName="TheResult" TaskParameter="NewString"/>
+    ///             <Output PropertyName="out" TaskParameter="NewString"/>
     ///         </MSBuild.ExtensionPack.Framework.TextString>
-    ///         <Message Text="The Result: $(TheResult)"/>
+    ///         <Message Text="The Result: $(out)"/>
     ///     </Target>
     /// </Project>
     /// ]]></code>
@@ -63,6 +99,7 @@ namespace MSBuild.ExtensionPack.Framework
     public class TextString : BaseTask
     {
         private bool ignoreCase = true;
+        private StringComparison strcom = StringComparison.OrdinalIgnoreCase;
 
         /// <summary>
         /// Sets the start index for Remove
@@ -70,7 +107,7 @@ namespace MSBuild.ExtensionPack.Framework
         public int StartIndex { get; set; }
 
         /// <summary>
-        /// Sets the count for Remove
+        /// Sets the count
         /// </summary>
         public int Count { get; set; }
 
@@ -135,10 +172,23 @@ namespace MSBuild.ExtensionPack.Framework
                 return;
             }
 
+            if (!this.IgnoreCase)
+            {
+                this.strcom = StringComparison.Ordinal;
+            }
+
             switch (this.TaskAction)
             {
                 case "Compare":
                     this.Compare();
+                    break;
+                case "EndsWith":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Checking whether: {0} ends with: {1}", this.String1, this.String2));
+                    this.Result = this.String1.EndsWith(this.String2, this.strcom);
+                    break;
+                case "StartsWith":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Checking whether: {0} starts with: {1}", this.String1, this.String2));
+                    this.Result = this.String1.StartsWith(this.String2, this.strcom);
                     break;
                 case "Replace":
                     this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Replacing String: {0}", this.OldString));
@@ -147,6 +197,14 @@ namespace MSBuild.ExtensionPack.Framework
                 case "Trim":
                     this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Trimming String: {0}", this.OldString));
                     this.NewString = this.OldString.Trim();
+                    break;
+                case "ToLower":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Lower casing: {0}", this.OldString));
+                    this.NewString = this.OldString.ToLower(CultureInfo.CurrentUICulture);
+                    break;
+                case "ToUpper":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Upper casing: {0}", this.OldString));
+                    this.NewString = this.OldString.ToUpper(CultureInfo.CurrentUICulture);
                     break;
                 case "Remove":
                     this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Removing String: {0}", this.OldString));
@@ -159,6 +217,14 @@ namespace MSBuild.ExtensionPack.Framework
                 case "Insert":
                     this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Inserting: {0} into: {1}", this.String1, this.OldString));
                     this.NewString = this.OldString.Insert(this.StartIndex, this.String1);
+                    break;
+                case "PadLeft":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Padding: {0} left with: {1}", this.OldString, this.String1[0]));
+                    this.NewString = this.OldString.PadLeft(this.Count, this.String1[0]);
+                    break;
+                case "PadRight":
+                    this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Padding: {0} right with: {1}", this.OldString, this.String1[0]));
+                    this.NewString = this.OldString.PadRight(this.Count, this.String1[0]);
                     break;
                 default:
                     this.Log.LogError(string.Format(CultureInfo.CurrentCulture, "Invalid TaskAction passed: {0}", this.TaskAction));
@@ -190,14 +256,6 @@ namespace MSBuild.ExtensionPack.Framework
             }
 
             this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Comparing String: {0} [{1}] {2}", this.String1, this.Comparison.ToUpperInvariant(), this.String2));
-
-            StringComparison strcom = StringComparison.Ordinal;
-
-            if (this.IgnoreCase)
-            {
-                strcom = StringComparison.OrdinalIgnoreCase;
-            }
-
             switch (this.Comparison.ToUpperInvariant())
             {
                 case "GREATERTHAN":
