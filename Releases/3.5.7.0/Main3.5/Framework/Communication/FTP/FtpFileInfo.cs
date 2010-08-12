0933f1541020127c9c4b0aefc129cc19e605cc25
hamid_shahid_cp <SND\hamid_shahid_cp>
2010-08-12 16:05:35 +0100

    Communication\Ftp.cs: New - Added FTP operations (3.5.7.0)

---
+++ Releases/3.5.7.0/Main3.5/Framework/Communication/FTP/FtpFileInfo.cs
@@ -0,0 +1,108 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FtpFileInfo.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace MSBuild.ExtensionPack.Communication
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Security.Permissions;
+
+    /// <summary>
+    /// The <c>FtpFileInfo</c> class encapsulates a remote FTP directory.
+    /// </summary>
+    [Serializable]
+    public sealed class FtpFileInfo : FileSystemInfo
+    {
+        private readonly string fileName; 
+        private readonly FtpConnection ftpConnection;
+
+        private DateTime? lastAccessTime;
+        private DateTime? lastWriteTime;
+        private DateTime? creationTime;
+
+        public FtpFileInfo(FtpConnection ftp, string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException("filePath");
+            }
+
+            OriginalPath = filePath;
+            FullPath = filePath;
+            this.ftpConnection = ftp;
+            this.fileName = Path.GetFileName(filePath);
+        }
+
+        private FtpFileInfo(SerializationInfo info, StreamingContext context) : base(info, context)
+        {         
+        }
+
+        public FtpConnection FtpConnection
+        {
+            get { return this.ftpConnection; }
+        }
+
+        public new DateTime? LastAccessTime
+        {
+            get { return this.lastAccessTime.HasValue ? (DateTime?)this.lastAccessTime.Value : null; }
+            internal set { this.lastAccessTime = value; }
+        }
+
+        public new DateTime? CreationTime
+        {
+            get { return this.creationTime.HasValue ? (DateTime?)this.creationTime.Value : null; }
+            internal set { this.creationTime = value; }
+        }
+
+        public new DateTime? LastWriteTime
+        {
+            get { return this.lastWriteTime.HasValue ? (DateTime?)this.lastWriteTime.Value : null; }
+            internal set { this.lastWriteTime = value; }
+        }
+
+        public new DateTime? LastAccessTimeUtc
+        {
+            get { return this.lastAccessTime.HasValue ? (DateTime?)this.lastAccessTime.Value.ToUniversalTime() : null; }
+        }
+
+        public new DateTime? CreationTimeUtc
+        {
+            get { return this.creationTime.HasValue ? (DateTime?)this.creationTime.Value.ToUniversalTime() : null; }
+        }
+
+        public new DateTime? LastWriteTimeUtc
+        {
+            get { return this.lastWriteTime.HasValue ? (DateTime?)this.lastWriteTime.Value.ToUniversalTime() : null; }
+        }
+
+        public new FileAttributes Attributes { get; internal set; }
+
+        public override string Name
+        {
+            get { return this.fileName; }
+        }
+
+        public override bool Exists
+        {
+            get { return this.FtpConnection.FileExists(this.FullName); }
+        }
+
+        public override void Delete()
+        {
+            this.FtpConnection.DeleteDirectory(this.FullName);
+        }
+
+        /// <summary>
+        /// No specific impelementation is needed of the GetObjectData to serialize this object
+        /// because all attributes are redefined.
+        /// </summary>
+        /// <param name="info">The SerializationInfo to populate with data. </param>
+        /// <param name="context">The destination for this serialization. </param>
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
