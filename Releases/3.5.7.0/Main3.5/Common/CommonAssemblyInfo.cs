85c2be8e4cf916ea123261e3a9c89b55f57daf86
mikeFourie_cp <SND\mikeFourie_cp>
2010-08-25 16:27:39 +0100

    MainSolution.sln and all projects updated to use VS 2010. (3.5.7.0)

--- Releases/3.5.7.0/Main3.5/Common/CommonAssemblyInfo.cs
+++ Releases/3.5.7.0/Main3.5/Common/CommonAssemblyInfo.cs
@@ -9,8 +9,6 @@ using System.Security.Permissions;
 [assembly:AssemblyVersion("3.5.0.0")]
 [assembly:AssemblyFileVersion("3.5.7.0")]
 [assembly:AssemblyInformationalVersion("3.5.0.0")]
-[assembly:PermissionSet(SecurityAction.RequestMinimum, Name = "FullTrust")]
-[assembly:PermissionSet(SecurityAction.RequestOptional, Name = "Nothing")]
 [assembly:AssemblyCompany("http://www.MSBuildExtensionPack.com")]
 [assembly:AssemblyCopyright("Copyright © 2010 http://www.MSBuildExtensionPack.com")]
 [assembly:AssemblyTrademark("Mike Fourie")]
