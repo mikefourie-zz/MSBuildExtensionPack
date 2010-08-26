b19837b20a476a75c9226f93190dde019a70f886
mikeFourie_cp <SND\mikeFourie_cp>
2010-08-26 13:30:03 +0100

    SqlServer\BuildDeploymentManifest.cs: New Task can be used to translate SSIS projects (.dtproj  files) into an SSIS deployment manifest. Based on http://sqlsrvintegrationsrv.codeplex.com and used with permission. (3.5.7.0 / 4.0.1.0)

--- Releases/4.0.1.0/Main/Framework/GlobalSuppressions.cs
+++ Releases/4.0.1.0/Main/Framework/GlobalSuppressions.cs
@@ -953,3 +953,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Scope = "member", Target = "MSBuild.ExtensionPack.Communication.Extended.FtpConnection.#GetCurrentDirectory()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1045:DoNotPassTypesByReference", MessageId = "0#", Scope = "member", Target = "MSBuild.ExtensionPack.Communication.Extended.FtpConnection.#InternetLastResponseInfo(System.Int32&)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "FxCop", Scope = "member", Target = "MSBuild.ExtensionPack.CodeQuality.FxCop.#InternalExecute()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity", Scope = "member", Target = "MSBuild.ExtensionPack.SqlServer.BuildDeploymentManifest.#Execute()")]
