1f2f0ec98437ee8d750d028d63383cd592e355f0
mikeFourie_cp <SND\mikeFourie_cp>
2010-08-27 15:39:22 +0100

    Branched from $/MSBuildExtensionPack/Solutions/Main

---
+++ Releases/4.0.2.0/BizTalk/BizTalkHelper.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BizTalkHelper.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace MSBuild.ExtensionPack.BizTalk
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// The available host states in BizTalk
+    /// </summary>
+    internal enum HostState
+    {
+        /// <summary>
+        /// Stopped
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Stopped
+        /// </summary>
+        Stopped = 1,
+
+        /// <summary>
+        /// StartPending
+        /// </summary>
+        StartPending = 2,
+
+        /// <summary>
+        /// StopPending
+        /// </summary>
+        StopPending = 3,
+
+        /// <summary>
+        /// Running
+        /// </summary>
+        Running = 4,
+
+        /// <summary>
+        /// Unknown
+        /// </summary>
+        Unknown = 8
+    }
+
+    /// <summary>
+    /// The available host types in BizTalk
+    /// </summary>
+    internal enum BizTalkHostType
+    {
+        /// <summary>
+        /// in-process (hosted in BizTalk)
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// in-process (hosted in BizTalk)
+        /// </summary>
+        InProcess = 1,
+
+        /// <summary>
+        /// out of process (not hosted in BizTalk, IIS etc)
+        /// </summary>
+        Isolated = 2
+    }
+
+    internal static class BizTalkHelper
+    {
+        /// <summary>
+        /// Checks whether a name is valid to use in BizTalk.
+        /// </summary>
+        /// <param name="name">Name to check</param>
+        /// <param name="invalidCharacters">string of invalid characters to match on</param>
+        /// <returns>boolean</returns>
+        public static bool IsValidName(string name, string invalidCharacters)
+        {
+            Regex r = new Regex(invalidCharacters, RegexOptions.Compiled);
+            Match m = r.Match(name);
+            return !m.Success;
+        }
+    }
+}
---
+++ Releases/4.0.2.0/Framework/Web/ComWrapper.cs
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------
+// <copyright file="ComWrapper.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace MSBuild.ExtensionPack.Web
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+
+    internal class ComWrapper
+    {
+        private object comObject;
+        private Type comObjectType;
+
+        public ComWrapper(object value)
+        {
+            this.Initialize(value);
+        }
+
+        internal object CallMethod(string methodName)
+        {
+            return this.CallMethod(methodName, new object[] { });
+        }
+
+        internal object CallMethod(string methodName, object[] args)
+        {
+            return this.comObjectType.InvokeMember(methodName, BindingFlags.Default | BindingFlags.InvokeMethod, null, this.comObject, args, CultureInfo.InvariantCulture);
+        }
+
+        private void Initialize(object value)
+        {
+            this.comObject = value;
+            this.comObjectType = this.comObject.GetType();
+        }
+    }
+}
