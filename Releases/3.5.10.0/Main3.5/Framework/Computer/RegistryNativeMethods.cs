de962370bdc128c49f812752c302b6ac3138bd3f
mikeFourie_cp <SND\mikeFourie_cp>
2011-04-19 12:45:56 +0100

    Branched from $/MSBuildExtensionPack/Solutions/Main3.5

---
+++ Releases/3.5.10.0/Main3.5/Framework/Computer/RegistryNativeMethods.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RegistryNativeMethods.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace MSBuild.ExtensionPack.Computer
+{
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    /// <summary>
+    /// RegistryNativeMethods
+    /// </summary>
+    [SuppressUnmanagedCodeSecurity]
+    internal static class NativeMethods
+    {
+        internal const int HWND_BROADCAST = 0xffff;
+        internal const int WM_SETTINGCHANGE = 0x001A;
+        internal const int SMTO_ABORTIFHUNG = 0x0002;
+
+        [DllImportAttribute("USER32", CharSet = CharSet.Unicode)]
+        internal static extern int SendMessageTimeout(int hWnd, int Msg, int wParam, string lParam, int fuFlags, int uTimeout, int lpdwResult);
+    }
+}
