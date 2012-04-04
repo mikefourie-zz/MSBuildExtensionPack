bd9737c238a335daa9a6e3659b5fcee93ee85792
mikeFourie_cp <SND\mikeFourie_cp>
2012-04-04 22:09:58 +0100

    A few CA suppressions for VS 11

--- Releases/4.0.6.0/Main/IIS7/GlobalSuppressions.cs
+++ Releases/4.0.6.0/Main/IIS7/GlobalSuppressions.cs
@@ -104,3 +104,4 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "MSBuild.ExtensionPack.Web.Iis7Website.#AddResponseHeaders()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "MimeType", Scope = "member", Target = "MSBuild.ExtensionPack.Web.Iis7Website.#AddMimeType()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Flags", Scope = "member", Target = "MSBuild.ExtensionPack.Web.Iis7Website.#LogExtFileFlags")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ip", Scope = "member", Target = "MSBuild.ExtensionPack.Web.Iis7Binding.#Add()")]
