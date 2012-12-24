13ae382ba09a619d0c7eca530bec607784b4d696
ISardaukar_cp <SND\ISardaukar_cp>
2012-12-24 15:09:53 +0000

    Initial implementation of Git Clone task.

---
+++ Solutions/Main/Git/IGitFacade.cs
@@ -0,0 +1,9 @@
+﻿namespace MSBuild.ExtensionPack.Git
+{
+    public interface IGitFacade
+    {
+        void Clone(string repositoryToClone, string targetDirectory);
+        void CheckoutBranch(string localRepository, string branch);
+        string GetLatestSha(string localRepository);
+    }
+}
