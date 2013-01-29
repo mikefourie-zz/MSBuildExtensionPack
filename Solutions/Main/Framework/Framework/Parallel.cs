3f0be5b8a4501f49fc5df8e9b35fdb851397e746
mikeFourie_cp <SND\mikeFourie_cp>
2013-01-29 12:09:33 +0000

    Framework\Parallel.cs: Add support for logging to multiple log files. New Properties are MultiLog, MultiLogOpenOnFailure, MultiLogVerbosity, MultiLogResponseVerbosity

--- Solutions/Main/Framework/Framework/Parallel.cs
+++ Solutions/Main/Framework/Framework/Parallel.cs
@@ -5,13 +5,14 @@ namespace MSBuild.ExtensionPack.Framework
 {
     using System;
     using System.Globalization;
+    using System.IO;
     using System.Linq;
     using Microsoft.Build.Framework;
 
     /// <summary>
     /// <b>Valid TaskActions are:</b>
-    /// <para><i>BuildTargetsInParallel</i> (<b>Required: </b> Targets <b>Optional:</b> AdditionalProperties, ProjectFile, WaitAll, WorkingDirectory)</para>
-    /// <para><i>BuildTargetSetsInParallel</i> (<b>Required: </b> Targets <b>Optional:</b> AdditionalProperties, ProjectFile, WaitAll, WorkingDirectory)</para>
+    /// <para><i>BuildTargetsInParallel</i> (<b>Required: </b> Targets <b>Optional:</b> AdditionalProperties, ProjectFile, WaitAll, WorkingDirectory, MultiLog, MultiLogOpenOnFailure, MultiLogVerbosity, MultiLogResponseVerbosity)</para>
+    /// <para><i>BuildTargetSetsInParallel</i> (<b>Required: </b> Targets <b>Optional:</b> AdditionalProperties, ProjectFile, WaitAll, WorkingDirectory, MultiLog, MultiLogOpenOnFailure, MultiLogVerbosity, MultiLogResponseVerbosity)</para>
     /// <para><b>Remote Execution Support:</b> NA</para>
     /// </summary>
     /// <example>
@@ -24,6 +25,8 @@ namespace MSBuild.ExtensionPack.Framework
     ///     <Import Project="$(TPath)"/>
     ///     <ItemGroup>
     ///         <MyTargetSets Include="1">
+    ///             <LogFilePath>C:\b</LogFilePath>
+    ///             <LogFileName>Target1yahoo.txt</LogFileName>
     ///             <Targets>Target1;Target2</Targets>
     ///             <Properties>MyPropValue=MyPropValue1</Properties>
     ///         </MyTargetSets>
@@ -33,14 +36,16 @@ namespace MSBuild.ExtensionPack.Framework
     ///         <MyTargets Include="Target1">
     ///             <Properties>MyPropValue=MyPropValue1</Properties>
     ///         </MyTargets>
-    ///         <MyTargets Include="Target2;Target3"/>
+    ///         <MyTargets Include="Target2;Target3">
+    ///             <LogFilePath>C:\b</LogFilePath>
+    ///         </MyTargets>
     ///     </ItemGroup>
     ///     <Target Name="Normal" DependsOnTargets="Target1;Target2;Target3"/>
     ///     <Target Name="BuildTargetSetsInParallel">
-    ///         <MSBuild.ExtensionPack.Framework.Parallel TaskAction="BuildTargetSetsInParallel" Targets="@(MyTargetSets)" AdditionalProperties="SkipInitial=true"/>
+    ///         <MSBuild.ExtensionPack.Framework.Parallel MultiLog="$(MultiLog)" MultiLogAppend="$(MultiLogAppend)" MultiLogOpenOnFailure="$(MultiLogOpenOnFailure)" TaskAction="BuildTargetSetsInParallel" Targets="@(MyTargetSets)"  AdditionalProperties="SkipInitial=true"/>
     ///     </Target>
     ///     <Target Name="BuildTargetsInParallel">
-    ///         <MSBuild.ExtensionPack.Framework.Parallel TaskAction="BuildTargetsInParallel" Targets="@(MyTargets)" AdditionalProperties="SkipInitial=true"/>
+    ///         <MSBuild.ExtensionPack.Framework.Parallel MultiLog="$(MultiLog)" MultiLogAppend="$(MultiLogAppend)" MultiLogOpenOnFailure="$(MultiLogOpenOnFailure)" TaskAction="BuildTargetsInParallel" Targets="@(MyTargets)" AdditionalProperties="SkipInitial=true"/>
     ///     </Target>
     ///     <Target Name="Target1">
     ///         <MSBuild.ExtensionPack.Framework.Thread TaskAction="Sleep" Timeout="1000"/>
@@ -64,14 +69,9 @@ namespace MSBuild.ExtensionPack.Framework
         private const string BuildTargetsInParallelTaskAction = "BuildTargetsInParallel";
         private const string BuildTargetSetsInParallelTaskAction = "BuildTargetSetsInParallel";
         private bool waitAll = true;
-
-        [DropdownValue(BuildTargetsInParallelTaskAction)]
-        [DropdownValue(BuildTargetSetsInParallelTaskAction)]
-        public override string TaskAction
-        {
-            get { return base.TaskAction; }
-            set { base.TaskAction = value; }
-        }
+        private bool multiLogAppend;
+        private LoggerVerbosity multiLogVerbosity = LoggerVerbosity.Diagnostic;
+        private LoggerVerbosity multiLogResponseVerbosity = LoggerVerbosity.Minimal;
 
         /// <summary>
         /// Specifies whether to wait for all Targets to complete execution before returning to MSBuild or whether to wait for all to complete. Default is true.
@@ -86,6 +86,46 @@ namespace MSBuild.ExtensionPack.Framework
         /// Specifies the working directory. Default is null and MSBuild is resolved to the Path environment variable.
         /// </summary>
         public string WorkingDirectory { get; set; }
+
+        /// <summary>
+        /// Specifies whether each parallel execution should log to it's own log file rather than the parent. Default is false.
+        /// For BuildTargetsInParallel you can specify a LogFilePath metadata value to define the root path to log to, 
+        /// otherwise they are written to the directory of the calling project. The name of the target is used as the file name.
+        /// For BuildTargetSetsInParallel you can specify a LogFilePath and a LogFileName metatdatavalue. If LogFileName is not passed, the target name is used.
+        /// </summary>
+        public bool MultiLog { get; set; }
+
+        /// <summary>
+        /// Specifies whether to open the log file containing the error info on failure. Default is false
+        /// </summary>
+        public bool MultiLogOpenOnFailure { get; set; }
+
+        /// <summary>
+        /// Specifies whether to append to existing log files. Default is false
+        /// </summary>
+        public bool MultiLogAppend
+        {
+            get { return this.multiLogAppend; }
+            set { this.multiLogAppend = value; }
+        }
+
+        /// <summary>
+        /// Specifies the verbosity to log to the individual files with. Default is diagnostic
+        /// </summary>
+        public string MultiLogVerbosity
+        {
+            get { return this.multiLogVerbosity.ToString(); }
+            set { this.multiLogVerbosity = (LoggerVerbosity)Enum.Parse(typeof(LoggerVerbosity), value); }
+        }
+
+        /// <summary>
+        /// Specifies the verbosity of logging fed back to the calling task. Default is Minimal
+        /// </summary>
+        public string MultiLogResponseVerbosity
+        {
+            get { return this.multiLogResponseVerbosity.ToString(); }
+            set { this.multiLogResponseVerbosity = (LoggerVerbosity)Enum.Parse(typeof(LoggerVerbosity), value); }
+        }
 
         /// <summary>
         /// Speficies the MSBuild project to use. Defaults to the calling MSBuild file.
@@ -108,6 +148,11 @@ namespace MSBuild.ExtensionPack.Framework
             if (!this.TargetingLocalMachine())
             {
                 return;
+            }
+
+            if (!this.MultiLog)
+            {
+                this.multiLogResponseVerbosity = LoggerVerbosity.Normal;
             }
 
             switch (this.TaskAction)
@@ -172,7 +217,35 @@ namespace MSBuild.ExtensionPack.Framework
             }
 
             string projectFile = this.ProjectFile == null ? this.BuildEngine.ProjectFileOfTaskNode : this.ProjectFile.ItemSpec;
-            var exec = new ShellWrapper("msbuild.exe", "\"" + projectFile + "\" /t:" + item.ItemSpec + properties);
+            string logginginfo = string.Empty;
+            string logfileName = item.GetMetadata("LogFilePath");
+            if (string.IsNullOrEmpty(logfileName))
+            {
+                logfileName = item.ItemSpec + ".txt";
+            }
+            else
+            {
+                if (!Directory.Exists(logfileName))
+                {
+                    Directory.CreateDirectory(logfileName);
+                }
+
+                logfileName = System.IO.Path.Combine(logfileName, item.ItemSpec + ".txt");
+            }
+
+            if (this.MultiLog)
+            {
+                // note there is a bug in MSBuild loggers whereby the logger will append whenever it sees append in the arguments, so you can's say append=false.
+                string append = string.Empty;
+                if (this.multiLogAppend)
+                {
+                    append = "append=true;";
+                }
+
+                logginginfo = string.Format(CultureInfo.CurrentCulture, "/l:FileLogger,Microsoft.Build.Engine;{0}verbosity={1};logfile={2}", append, this.MultiLogVerbosity, logfileName);
+            }
+
+            var exec = new ShellWrapper("msbuild.exe", "\"" + projectFile + "\" /v:" + this.MultiLogResponseVerbosity + " /t:" + item.ItemSpec + properties + " " + logginginfo);
             if (string.IsNullOrEmpty(this.WorkingDirectory) == false)
             {
                 exec.WorkingDirectory = this.WorkingDirectory;
@@ -190,29 +263,41 @@ namespace MSBuild.ExtensionPack.Framework
                                           };
 
             exec.OutputDataReceived += (sender, e) =>
-                                           {
-                                               if (e.Data != null)
-                                               {
-                                                   if (this.Log != null)
-                                                   {
-                                                       try
-                                                       {
-                                                           this.Log.LogMessage(MessageImportance.Normal, e.Data);
-                                                       }
-                                                       catch
-                                                       {
-                                                           // do nothing. We have a race condition here with the MSBuild host being killed and the logging still trying to occur.
-                                                       }
-                                                   }
-                                               }
-                                           };
+                {
+                    if (e.Data != null)
+                    {
+                        if (this.Log != null)
+                        {
+                            try
+                            {
+                                this.Log.LogMessage(MessageImportance.Normal, e.Data);
+                            }
+                            catch
+                            {
+                                // do nothing. We have a race condition here with the MSBuild host being killed and the logging still trying to occur.
+                            }
+                        }
+                    }
+                };
 
             // execute the process
             int exitCode = exec.Execute();
 
             if (exitCode != 0)
             {
-                this.Log.LogError(string.Format(CultureInfo.InvariantCulture, "Error Code: {0}", exitCode));
+                string errorFile = string.Empty;
+                if (this.MultiLog)
+                {
+                    errorFile = "Additional information may be found in: " + logfileName;
+                }
+
+                this.Log.LogError(string.Format(CultureInfo.InvariantCulture, "Error Code: {0}. {1}", exitCode, errorFile));
+
+                if (this.MultiLog && this.MultiLogOpenOnFailure)
+                {
+                    FileInfo f = new FileInfo(this.BuildEngine.ProjectFileOfTaskNode);
+                    System.Diagnostics.Process.Start(System.IO.Path.Combine(f.Directory.FullName, logfileName));
+                }
             }
         }
 
@@ -262,7 +347,40 @@ namespace MSBuild.ExtensionPack.Framework
             }
 
             string projectFile = this.ProjectFile == null ? this.BuildEngine.ProjectFileOfTaskNode : this.ProjectFile.ItemSpec;
-            var exec = new ShellWrapper("msbuild.exe", "\"" + projectFile + "\" /t:" + item.GetMetadata("Targets") + properties);
+            
+            string logginginfo = string.Empty;
+            string logfileName = item.GetMetadata("LogFileName");
+            if (string.IsNullOrEmpty(logfileName))
+            {
+                logfileName = item.ItemSpec + ".txt";
+            }
+            else
+            {
+                string logfilePath = item.GetMetadata("LogFilePath");
+                if (!string.IsNullOrEmpty(logfilePath))
+                {
+                    if (!Directory.Exists(logfilePath))
+                    {
+                        Directory.CreateDirectory(logfilePath);
+                    }
+
+                    logfileName = System.IO.Path.Combine(logfilePath, logfileName);
+                }
+            }
+
+            if (this.MultiLog)
+            {
+                // note there is a bug in MSBuild loggers whereby the logger will append whenever it sees append in the arguments, so you can's say append=false.
+                string append = string.Empty;
+                if (this.multiLogAppend)
+                {
+                    append = "append=true;";
+                }
+
+                logginginfo = string.Format(CultureInfo.CurrentCulture, "/l:FileLogger,Microsoft.Build.Engine;{0}verbosity={1};logfile={2}", append, this.MultiLogVerbosity, logfileName);
+            }
+
+            var exec = new ShellWrapper("msbuild.exe", "\"" + projectFile + "\" /v:" + this.MultiLogResponseVerbosity + " /t:" + item.GetMetadata("Targets") + properties + " " + logginginfo);
             if (string.IsNullOrEmpty(this.WorkingDirectory) == false)
             {
                 exec.WorkingDirectory = this.WorkingDirectory;
@@ -280,28 +398,40 @@ namespace MSBuild.ExtensionPack.Framework
                                           };
 
             exec.OutputDataReceived += (sender, e) =>
-                                           {
-                                               if (e.Data != null)
-                                               {
-                                                   if (this.Log != null)
-                                                   {
-                                                       try
-                                                       {
-                                                           this.Log.LogMessage(MessageImportance.Normal, e.Data);
-                                                       }
-                                                       catch
-                                                       {
-                                                           // do nothing. We have a race condition here with the MSBuild host being killed and the logging still trying to occur.
-                                                       }
-                                                   }
-                                               }
-                                           };
+                {
+                    if (e.Data != null)
+                    {
+                        if (this.Log != null)
+                        {
+                            try
+                            {
+                                this.Log.LogMessage(MessageImportance.Normal, e.Data);
+                            }
+                            catch
+                            {
+                                // do nothing. We have a race condition here with the MSBuild host being killed and the logging still trying to occur.
+                            }
+                        }
+                    }
+                };
 
             // execute the process
             int exitCode = exec.Execute();
             if (exitCode != 0)
             {
-                this.Log.LogError(string.Format(CultureInfo.InvariantCulture, "Error Code: {0}", exitCode));
+                string errorFile = string.Empty;
+                if (this.MultiLog)
+                {
+                    errorFile = "Additional information may be found in: " + logfileName;
+                }
+
+                this.Log.LogError(string.Format(CultureInfo.InvariantCulture, "Error Code: {0}. {1}", exitCode, errorFile));
+
+                if (this.MultiLog && this.MultiLogOpenOnFailure)
+                {
+                    FileInfo f = new FileInfo(this.BuildEngine.ProjectFileOfTaskNode);
+                    System.Diagnostics.Process.Start(System.IO.Path.Combine(f.Directory.FullName, logfileName));
+                }
             }
         }
     }
