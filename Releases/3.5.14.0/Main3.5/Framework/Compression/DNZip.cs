b50fe29b1491873f2a143ad6307fee74cd96fdf0
mikeFourie_cp <SND\mikeFourie_cp>
2013-10-09 22:15:29 +0100

    Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5

---
+++ Releases/3.5.14.0/Main3.5/Framework/Compression/DNZip.cs
@@ -0,0 +1,380 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DNZip.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+#pragma warning disable 618
+namespace MSBuild.ExtensionPack.Compression
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using Ionic.Zip;
+    using Microsoft.Build.Framework;
+
+    /// <summary>
+    /// <para>NOTE: This task is for backwards compatibility only. You should use the Zip task rather</para>
+    /// <b>Valid TaskActions are:</b>
+    /// <para><i>AddFiles</i> (<b>Required: </b> ZipFileName, CompressFiles or Path <b>Optional: </b>CompressionLevel, MaxOutputSegmentSize, Password; RemoveRoot, UseZip64WhenSaving) Existing files will be updated</para>
+    /// <para><i>Create</i> (<b>Required: </b> ZipFileName, CompressFiles or Path <b>Optional: </b>CompressionLevel, MaxOutputSegmentSize, Password; RemoveRoot, UseZip64WhenSaving)</para>
+    /// <para><i>Extract</i> (<b>Required: </b> ZipFileName, ExtractPath <b>Optional:</b> Password)</para>
+    /// <para><b>Remote Execution Support:</b> NA</para>
+    /// <para/>
+    /// This task uses http://dotnetzip.codeplex.com v1.9.1.8 for compression.
+    /// <para/>
+    /// </summary>
+    /// <example>
+    /// <code lang="xml"><![CDATA[
+    /// <Project ToolsVersion="3.5" DefaultTargets="Default" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+    ///   <PropertyGroup>
+    ///     <TPath>$(MSBuildProjectDirectory)\..\MSBuild.ExtensionPack.tasks</TPath>
+    ///     <TPath Condition="Exists('$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks')">$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks</TPath>
+    ///   </PropertyGroup>
+    ///   <Import Project="$(TPath)"/>
+    ///   <Target Name="Default" DependsOnTargets="Sample1;Sample2"/>
+    ///   <Target Name="Sample1">
+    ///     <ItemGroup>
+    ///       <!-- Set the collection of files to Zip-->
+    ///       <FilesToZip Include="C:\Patches\**\*"/>
+    ///     </ItemGroup>
+    ///     <!-- Create a zip file based on the FilesToZip collection -->
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Create" CompressFiles="@(FilesToZip)" ZipFileName="C:\newZipByFile.zip"/>
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Create" Password="apassword" CompressionLevel="BestCompression" RemoveRoot="C:\Patches" CompressFiles="@(FilesToZip)" ZipFileName="C:\newZipByFileBestCompression.zip"/>
+    ///     <!-- Create a zip file based on a Path -->
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Create" CompressPath="C:\Patches" ZipFileName="C:\newZipByPath.zip"/>
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Create" CompressPath="C:\Patches" ZipFileName="C:\newZipByPath.zip" MaxOutputSegmentSize="734003200" UseZip64WhenSaving="AsNecessary"/>
+    ///     <!-- Extract a zip file-->
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Extract" ExtractPath="C:\aaa11\1" ZipFileName="C:\newZipByFile.zip"/>
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Extract" ExtractPath="C:\aaa11\2" ZipFileName="C:\newZipByPath.zip"/>
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Extract" Password="apassword"  ExtractPath="C:\aaa11\3" ZipFileName="C:\newZipByFileBestCompression.zip"/>
+    ///   </Target>
+    ///   <Target Name="Sample2">
+    ///     <PropertyGroup>
+    ///       <SourceDirectory>MotorData\</SourceDirectory>
+    ///     </PropertyGroup>
+    ///     <ItemGroup>
+    ///       <Files Include="$(SourceDirectory)*" Exclude="$(SourceDirectory).XYZ\**\*">
+    ///         <Group>Common</Group>
+    ///       </Files>
+    ///       <Files Include="$(SourceDirectory)Cars\*" Exclude="$(SourceDirectory)Cars\.XYZ\**\*">
+    ///         <Group>Cars</Group>
+    ///       </Files>
+    ///       <Files Include="$(SourceDirectory)Trucks\*" Exclude="$(SourceDirectory)Trucks\.XYZ\**\*">
+    ///         <Group>Trucks</Group>
+    ///       </Files>
+    ///     </ItemGroup>
+    ///     <!-- Create the output folder -->
+    ///     <ItemGroup>
+    ///       <OutputDirectory Include="output\"/>
+    ///     </ItemGroup>
+    ///     <MakeDir Directories="@(OutputDirectory)"/>
+    ///     <PropertyGroup>
+    ///       <WorkingDir>%(OutputDirectory.Fullpath)</WorkingDir>
+    ///     </PropertyGroup>
+    ///     <!-- Zip files based on the group they belong to -->
+    ///     <MSBuild.ExtensionPack.Compression.DNZip TaskAction="Create" CompressFiles="@(Files)" ZipFileName="$(WorkingDir)%(Files.Group).zip"/>
+    ///   </Target>
+    /// </Project>
+    /// ]]></code>    
+    /// </example>  
+    [HelpUrl("http://www.msbuildextensionpack.com/help/3.5.12.0/html/15d161fa-cd51-6f4e-e6ef-99c757b517c4.htm")]
+    public class DNZip : BaseTask
+    {
+        private const string CreateTaskAction = "Create";
+        private const string ExtractTaskAction = "Extract";
+        private const string AddFilesTaskAction = "AddFiles";
+        private Ionic.Zlib.CompressionLevel compressLevel = Ionic.Zlib.CompressionLevel.Default;
+        private Zip64Option useZip64WhenSaving = Zip64Option.Default;
+
+        /// <summary>
+        /// Sets the TaskAction.
+        /// </summary>
+        [DropdownValue(CreateTaskAction)]
+        [DropdownValue(ExtractTaskAction)]
+        [DropdownValue(AddFilesTaskAction)]
+        public override string TaskAction
+        {
+            get { return base.TaskAction; }
+            set { base.TaskAction = value; }
+        }
+
+        /// <summary>
+        /// Sets the root to remove from the zip path. Note that this should be part of the file to compress path, not the target path of the ZipFileName
+        /// </summary>
+        [TaskAction(CreateTaskAction, false)]
+        public ITaskItem RemoveRoot { get; set; }
+
+        /// <summary>
+        /// Sets the files to Compress
+        /// </summary>
+        [TaskAction(CreateTaskAction, false)]
+        public ITaskItem[] CompressFiles { get; set; }
+
+        /// <summary>
+        /// Sets the Path to Zip.
+        /// </summary>
+        [TaskAction(CreateTaskAction, false)]
+        public ITaskItem CompressPath { get; set; }
+
+        /// <summary>
+        /// Sets the name of the Zip File
+        /// </summary>
+        [Required]
+        [TaskAction(CreateTaskAction, true)]
+        [TaskAction(ExtractTaskAction, true)]
+        [TaskAction(AddFilesTaskAction, true)]
+        public ITaskItem ZipFileName { get; set; }
+
+        /// <summary>
+        /// Path to extract the zip file to
+        /// </summary>
+        [TaskAction(ExtractTaskAction, true)]
+        public ITaskItem ExtractPath { get; set; }
+
+        /// <summary>
+        /// Sets the Password to be used
+        /// </summary>
+        [TaskAction(CreateTaskAction, true)]
+        [TaskAction(ExtractTaskAction, true)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Sets the CompressionLevel to use. Default is Default, also supports BestSpeed and BestCompression
+        /// </summary>
+        [TaskAction(CreateTaskAction, true)]
+        public string CompressionLevel
+        {
+            get { return this.compressLevel.ToString(); }
+            set { this.compressLevel = (Ionic.Zlib.CompressionLevel)Enum.Parse(typeof(Ionic.Zlib.CompressionLevel), value); }
+        }
+
+        /// <summary>
+        /// Sets the maximum output segment size, which typically results in a split archive (an archive split into multiple files).
+        /// This value is not required and if not set or set to 0 the resulting archive will not be split.
+        /// For more details see the DotNetZip documentation.
+        /// </summary>
+        [TaskAction(CreateTaskAction, false)]
+        [TaskAction(AddFilesTaskAction, false)]
+        public int MaxOutputSegmentSize { get; set; }
+        
+        /// <summary>
+        /// Sets the UseZip64WhenSaving output of the DotNetZip library.
+        /// For more details see the DotNetZip documentation.
+        /// </summary>
+        [TaskAction(CreateTaskAction, false)]
+        [TaskAction(AddFilesTaskAction, false)]
+        public string UseZip64WhenSaving 
+        {
+            get { return this.useZip64WhenSaving.ToString(); }
+            set { this.useZip64WhenSaving = (Zip64Option)Enum.Parse(typeof(Zip64Option), value, true); }
+        }
+
+        /// <summary>
+        /// This is the main InternalExecute method that all tasks should implement
+        /// </summary>
+        protected override void InternalExecute()
+        {
+            if (!this.TargetingLocalMachine())
+            {
+                return;
+            }
+
+            switch (this.TaskAction)
+            {
+                case CreateTaskAction:
+                    this.Create();
+                    break;
+                case ExtractTaskAction:
+                    this.Extract();
+                    break;
+                case AddFilesTaskAction:
+                    this.AddFiles();
+                    break;
+                default:
+                    this.Log.LogError(string.Format(CultureInfo.CurrentCulture, "Invalid TaskAction passed: {0}", this.TaskAction));
+                    return;
+            }
+        }
+
+        private void AddFiles()
+        {
+            this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Adding files to ZipFile: {0}", this.ZipFileName));
+            if (this.CompressFiles != null)
+            {
+                using (ZipFile zip = ZipFile.Read(this.ZipFileName.ItemSpec))
+                {
+                    zip.ParallelDeflateThreshold = -1;
+                    zip.UseUnicodeAsNecessary = true;
+                    zip.CompressionLevel = this.compressLevel;
+                    if (!string.IsNullOrEmpty(this.Password))
+                    {
+                        zip.Password = this.Password;
+                    }
+
+                    foreach (ITaskItem f in this.CompressFiles)
+                    {
+                        if (this.RemoveRoot != null)
+                        {
+                            string location = (f.GetMetadata("RootDir") + f.GetMetadata("Directory")).Replace(this.RemoveRoot.GetMetadata("FullPath"), string.Empty);
+                            zip.UpdateFile(f.GetMetadata("FullPath"), location);
+                        }
+                        else
+                        {
+                            zip.UpdateFile(f.GetMetadata("FullPath"));
+                        }
+                    }
+
+                    if (this.MaxOutputSegmentSize > 0)
+                    {
+                        zip.MaxOutputSegmentSize = this.MaxOutputSegmentSize;
+                    }
+
+                    zip.UseZip64WhenSaving = this.useZip64WhenSaving;
+                    zip.Save();
+                }
+            }
+            else if (this.CompressPath != null)
+            {
+                using (ZipFile zip = ZipFile.Read(this.ZipFileName.ItemSpec))
+                {
+                    zip.ParallelDeflateThreshold = -1;
+                    zip.UseUnicodeAsNecessary = true;
+                    zip.CompressionLevel = this.compressLevel;
+                    if (!string.IsNullOrEmpty(this.Password))
+                    {
+                        zip.Password = this.Password;
+                    }
+
+                    if (this.RemoveRoot != null)
+                    {
+                        DirectoryInfo d = new DirectoryInfo(this.CompressPath.ItemSpec);
+                        string location = d.FullName.Replace(this.RemoveRoot.GetMetadata("FullPath"), string.Empty);
+                        zip.AddDirectory(this.CompressPath.ItemSpec, location);
+                    }
+                    else
+                    {
+                        zip.UpdateDirectory(this.CompressPath.ItemSpec);
+                    }
+
+                    if (this.MaxOutputSegmentSize > 0)
+                    {
+                        zip.MaxOutputSegmentSize = this.MaxOutputSegmentSize;
+                    }
+
+                    zip.UseZip64WhenSaving = this.useZip64WhenSaving;
+                    zip.Save();
+                }
+            }
+            else
+            {
+                Log.LogError("CompressFiles or CompressPath must be specified");
+                return;
+            }
+        }
+
+        private void Create()
+        {
+            this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Creating ZipFile: {0}", this.ZipFileName));
+            if (this.CompressFiles != null)
+            {
+                using (ZipFile zip = new ZipFile())
+                {
+                    zip.ParallelDeflateThreshold = -1;
+                    zip.UseUnicodeAsNecessary = true;
+                    zip.CompressionLevel = this.compressLevel;
+                    if (!string.IsNullOrEmpty(this.Password))
+                    {
+                        zip.Password = this.Password;
+                    }
+
+                    foreach (ITaskItem f in this.CompressFiles)
+                    {
+                        if (this.RemoveRoot != null)
+                        {
+                            string location = (f.GetMetadata("RootDir") + f.GetMetadata("Directory")).Replace(this.RemoveRoot.GetMetadata("FullPath"), string.Empty);
+                            zip.AddFile(f.GetMetadata("FullPath"), location);
+                        }
+                        else
+                        {
+                            zip.AddFile(f.GetMetadata("FullPath"));
+                        }
+                    }
+
+                    if (this.MaxOutputSegmentSize > 0)
+                    {
+                        zip.MaxOutputSegmentSize = this.MaxOutputSegmentSize;
+                    }
+
+                    zip.UseZip64WhenSaving = this.useZip64WhenSaving;
+                    zip.Save(this.ZipFileName.ItemSpec);
+                }
+            }
+            else if (this.CompressPath != null)
+            {
+                using (ZipFile zip = new ZipFile())
+                {
+                    zip.ParallelDeflateThreshold = -1;
+                    zip.UseUnicodeAsNecessary = true;
+                    zip.CompressionLevel = this.compressLevel;
+                    if (!string.IsNullOrEmpty(this.Password))
+                    {
+                        zip.Password = this.Password;
+                    }
+
+                    if (this.RemoveRoot != null)
+                    {
+                        DirectoryInfo d = new DirectoryInfo(this.CompressPath.ItemSpec);
+                        string location = d.FullName.Replace(this.RemoveRoot.GetMetadata("FullPath"), string.Empty);
+                        zip.AddDirectory(this.CompressPath.ItemSpec, location);
+                    }
+                    else
+                    {
+                        DirectoryInfo d = new DirectoryInfo(this.CompressPath.ItemSpec);
+                        zip.AddDirectory(this.CompressPath.ItemSpec, d.Name);
+                    }
+
+                    if (this.MaxOutputSegmentSize > 0)
+                    {
+                        zip.MaxOutputSegmentSize = this.MaxOutputSegmentSize;
+                    }
+
+                    zip.UseZip64WhenSaving = this.useZip64WhenSaving;
+                    zip.Save(this.ZipFileName.ItemSpec);
+                }
+            }
+            else
+            {
+                Log.LogError("CompressFiles or CompressPath must be specified");
+                return;
+            }
+        }
+
+        private void Extract()
+        {
+            if (!File.Exists(this.ZipFileName.GetMetadata("FullPath")))
+            {
+                Log.LogError(string.Format(CultureInfo.CurrentCulture, "ZipFileName not found: {0}", this.ZipFileName));
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.ExtractPath.GetMetadata("FullPath")))
+            {
+                Log.LogError("ExtractPath is required");
+                return;
+            }
+
+            this.LogTaskMessage(string.Format(CultureInfo.CurrentCulture, "Extracting ZipFile: {0} to: {1}", this.ZipFileName, this.ExtractPath));
+
+            using (ZipFile zip = ZipFile.Read(this.ZipFileName.GetMetadata("FullPath")))
+            {
+                if (!string.IsNullOrEmpty(this.Password))
+                {
+                    zip.Password = this.Password;
+                }
+
+                foreach (ZipEntry e in zip)
+                {
+                    e.Extract(this.ExtractPath.GetMetadata("FullPath"), ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }
+    }
+}
---
+++ Releases/3.5.14.0/Main3.5/Framework/Computer/HostsFile/HostsFile.cs
@@ -0,0 +1,226 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HostsFile.cs">(c) http://www.codeplex.com/MSBuildExtensionPack. This source is subject to the Microsoft Permissive License. See http://www.microsoft.com/resources/sharedsource/licensingbasics/sharedsourcelicenses.mspx. All other rights reserved.</copyright>
+//-----------------------------------------------------------------------
+namespace MSBuild.ExtensionPack.Computer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    /// <summary>
+    /// <b>Valid TaskActions are:</b>
+    /// <para><i>SetHostEntry</i> (<b>Required: </b> HostName, IPAddress<b> Optional: </b>Comment, PathToHostsFile)</para>
+    /// <para><i>Update</i> (<b>Required: </b>HostEntries <b>Optional: </b>PathToHostsFile, Truncate</para>
+    /// <para><b>Remote Execution Support:</b> No</para>
+    /// </summary>
+    /// <example>
+    /// <code lang="xml"><![CDATA[
+    /// <Project ToolsVersion="4.0" DefaultTargets="Default" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+    ///     <PropertyGroup>
+    ///         <TPath>$(MSBuildProjectDirectory)\..\MSBuild.ExtensionPack.tasks</TPath>
+    ///         <TPath Condition="Exists('$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks')">$(MSBuildProjectDirectory)\..\..\Common\MSBuild.ExtensionPack.tasks</TPath>
+    ///     </PropertyGroup>
+    ///     <Import Project="$(TPath)"/>    
+    ///     <ItemGroup>
+    ///         <HostEntries Include="MyWebService">
+    ///             <IPAddress>10.0.0.1</IPAddress>
+    ///             <Comment>The IP address for MyWebService</Comment>
+    ///         </HostEntries>
+    ///         <HostEntries Include="MyWebSite">
+    ///             <IPAddress>10.0.0.2</IPAddress>
+    ///         </HostEntries>
+    ///     </ItemGroup>
+    ///     <Target Name="Default">
+    ///         <!-- Update the current machine's hosts file.  With Truncate=True, any hosts entries not in @(HostEntries) will
+    ///        be deleted (except for the default localhost/127.0.0.1 entry). -->
+    ///         <MSBuild.ExtensionPack.Computer.HostsFile TaskAction="Update"
+    ///                                                   HostEntries="@(HostEntries)"
+    ///                                                   Truncate="True" />
+    ///         <!-- Update a hosts file in a custom location.  -->
+    ///         <MSBuild.ExtensionPack.Computer.HostsFile TaskAction="Update"
+    ///                                                   HostEntries="@(HostEntries)"
+    ///                                                   PathToHostsFile="\\SDG-WKS1348\a\hosts" />
+    ///         <!-- Update a single host entry.  If the entry doesn't exist, it will be created. -->
+    ///         <MSBuild.ExtensionPack.Computer.HostsFile TaskAction="SetHostEntry"
+    ///                                                   HostName="MyInternalHost"
+    ///                                                   IPAddress="10.0.0.3"
+    ///                                                   Comment="This points to the MyInternalHost server." />
+    ///     </Target>
+    /// </Project>
+    /// ]]></code>
+    /// </example>
+    [HelpUrl("http://www.msbuildextensionpack.com/help/3.5.12.0/html/8d85822b-beee-98e5-1ecb-7370ed8974f2.htm")]
+    public sealed class HostsFile : BaseTask
+    {
+        internal const string SetHostEntryTaskAction = "SetHostEntry";
+        internal const string UpdateTaskAction = "Update";
+        private readonly IHostsFileReader hostsFileReader;
+        private readonly IHostsFileWriter hostsFileWriter;
+
+        public HostsFile() : this(new HostsFileReader(), new HostsFileWriter())
+        {
+        }
+
+        internal HostsFile(IHostsFileReader hostsFileReader, IHostsFileWriter hostsFileWriter)
+        {
+            this.hostsFileReader = hostsFileReader;
+            this.hostsFileWriter = hostsFileWriter;
+        }
+
+        /// <summary>
+        /// The comment after the hosts entry.  Only used by the SetHostEntry task action.
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// The hostname to alias.  Only used by the SetHostEntry task action.
+        /// </summary>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// The IP address for the hosts entry being aliased.  Required.  Only used by the SetHostEntry task action.
+        /// </summary>
+        public string IPAddress { get; set; }
+
+        /// <summary>
+        /// If True, any host entry not in the HostEntries item group will be removed from the hosts file.  Default is
+        /// False.  Only used by the Update task action.
+        /// </summary>
+        public bool Truncate { get; set; }
+
+        /// <summary>
+        /// The list of hosts entries.  The identity should be the host name.  The IP address should be in the IPAddress
+        /// metadata.  A comment about the entry should be in the Comment metadata.  The task will fail if the identity or
+        /// IPAddress metadata are empty.
+        /// </summary>
+        public ITaskItem[] HostEntries { get; set; }
+
+        /// <summary>
+        /// The path to the hosts file to update.  Defaults to %SYSTEMROOT%\system32\drivers\etc\hosts.  Task will fail
+        /// if this file doesn't exist.
+        /// </summary>
+        public string PathToHostsFile { get; set; }
+
+        protected override void InternalExecute()
+        {
+            var pathToHostsFile = this.PathToHostsFile;
+            if (string.IsNullOrEmpty(pathToHostsFile))
+            {
+                pathToHostsFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
+                this.LogTaskMessage(MessageImportance.Low, string.Format(CultureInfo.InvariantCulture, "Path to hosts file is empty; defaulting to {0}.", pathToHostsFile));
+            }
+
+            if (!File.Exists(pathToHostsFile))
+            {
+                this.Log.LogError("Unable to find hosts file at '{0}'.", pathToHostsFile);
+                return;
+            }
+
+            List<ITaskItem> hostEntries;
+            var truncate = this.Truncate;
+
+            switch (this.TaskAction)
+            {
+                case UpdateTaskAction:
+                    if (this.HostEntries == null || this.HostEntries.Length == 0)
+                    {
+                        this.Log.LogError("HostsEntries property is empty or missing.");
+                        return;
+                    }
+
+                    hostEntries = new List<ITaskItem>(this.HostEntries);
+                    break;
+                case SetHostEntryTaskAction:
+                    // not allowed to truncate when only updating one host entry
+                    truncate = false;
+                    var hostEntry = new TaskItem(this.HostName);
+                    hostEntry.SetMetadata("IPAddress", this.IPAddress);
+                    if (!string.IsNullOrEmpty(this.Comment))
+                    {
+                        hostEntry.SetMetadata("Comment", this.Comment);
+                    }
+
+                    hostEntries = new List<ITaskItem>(new[] { hostEntry });
+                    break;
+                default:
+                    this.Log.LogError(string.Format(CultureInfo.CurrentCulture, "Invalid TaskAction passed: {0}", this.TaskAction));
+                    return;
+            }
+
+            this.LogTaskMessage(MessageImportance.Low, string.Format(CultureInfo.InvariantCulture, "Opening hosts file {0}.", pathToHostsFile));
+            var hostsFile = this.hostsFileReader.Read(pathToHostsFile, truncate);
+
+            if (hostEntries.Any(hostEntry => !this.SetHostEntry(hostEntry, hostsFile)))
+            {
+                return;
+            }
+
+            bool changedAttribute = false;
+
+            try
+            {
+                this.LogTaskMessage(MessageImportance.Normal, string.Format(CultureInfo.InvariantCulture, "Writing changes to {0}.", pathToHostsFile));
+
+                FileAttributes fileAttributes = System.IO.File.GetAttributes(pathToHostsFile);
+
+                // If readonly attribute is set, reset it.
+                if ((fileAttributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    this.LogTaskMessage(MessageImportance.Low, "Making file writable");
+                    System.IO.File.SetAttributes(pathToHostsFile, fileAttributes ^ FileAttributes.ReadOnly);
+                    changedAttribute = true;
+                }
+
+                this.hostsFileWriter.Write(pathToHostsFile, hostsFile);
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogError("An error occurred updating the host file at '{0}'.", this.PathToHostsFile);
+                this.Log.LogErrorFromException(ex);
+            }
+            finally
+            {
+                if (changedAttribute)
+                {
+                    this.LogTaskMessage(MessageImportance.Low, "Making file readonly");
+                    System.IO.File.SetAttributes(pathToHostsFile, FileAttributes.ReadOnly);
+                }
+            }
+        }
+
+        private bool SetHostEntry(ITaskItem hostEntry, IHostsFile hostsFile)
+        {
+            var ipAddress = hostEntry.GetMetadata("IPAddress");
+            var hostName = hostEntry.ItemSpec;
+            var comment = hostEntry.GetMetadata("Comment");
+
+            if (string.IsNullOrEmpty(hostName))
+            {
+                this.Log.LogError("HostName is null or empty.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                this.Log.LogError("IPAddress is null or empty for hostname '{0}.", hostName);
+                return false;
+            }
+
+            IPAddress parsedIPAddress;
+            if (!System.Net.IPAddress.TryParse(ipAddress, out parsedIPAddress))
+            {
+                this.Log.LogError("Invalid IP address ({0}) for hostname '{1}'.", ipAddress, hostName);
+                return false;
+            }
+
+            this.LogTaskMessage(MessageImportance.Normal, string.Format(CultureInfo.InvariantCulture, "Updating hosts entry for host {0} to IP address {1}.", hostName, ipAddress));
+            hostsFile.SetHostEntry(hostName, ipAddress, comment);
+            return true;
+        }
+    }
+}
